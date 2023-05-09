569f367589f84b272e199b622d1a25e46db4d322
Jake Stanton <jaccobstanton@gmail.com>
2023-05-09 14:01:09 -0500

    bootstrap implementation begins

--- src/scenes/imager_settings.jsx
+++ src/scenes/imager_settings.jsx
@@ -1,10 +1,49 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Imager_Settings() {
   return (
-    <div>
-      <h1>hi</h1>
-    </div>
+    <Container>
+      <Row>
+        <Col
+          style={{
+            backgroundColor: "#ccc",
+            height: "200px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          Box 1
+        </Col>
+      </Row>
+      <Row>
+        <Col
+          style={{
+            backgroundColor: "#ccc",
+            height: "200px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          Box 2
+        </Col>
+      </Row>
+      <Row>
+        <Col
+          style={{
+            backgroundColor: "#ccc",
+            height: "200px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          Box 3
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
