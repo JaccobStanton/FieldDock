b84c694581e03a05d2cc3f3208e07a74f10f2ec7
Jake Stanton <jaccobstanton@gmail.com>
2023-06-26 17:34:08 -0500

    working on toggle image type page

--- src/scenes/dashboard/imager_settings.jsx
+++ src/scenes/dashboard/imager_settings.jsx
@@ -1,11 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
 import React from "react";
-import { Link } from "react-router-dom";
-import { Box, ListItemIcon } from "@mui/material";
-import { useState, useEffect } from "react";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
-import { ExpandMore } from "@mui/icons-material";
 import CommonRow from "../../components/CommonRow";
 import "../../CSS/imager_settings.css";
 // import { ReactComponent as DroneDiagram } from "../../assets/Drone-Diagram.svg";
@@ -31,6 +25,21 @@ function Imager_Settings() {
           </div>
         </Col>
         {/* -------------------------------------------------PARENT column row 2 END ----------------------------- */}
+      </Row>
+
+      <Row>
+        <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12}>
+          <div className="largeBox">
+            <select className="dropdown" defaultValue="00:00PM">
+              <option disabled>00:00PM</option>
+              <option>1:00PM</option>
+              <option>2:00PM</option>
+              <option>3:00PM</option>
+              <option>4:00PM</option>
+              <option>5:00PM</option>
+            </select>
+          </div>
+        </Col>
       </Row>
     </Container>
   );
