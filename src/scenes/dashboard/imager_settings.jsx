b4a4d4a0f3b61f89a62e99b7d2c88be243ce59fc
Jake Stanton <jaccobstanton@gmail.com>
2023-06-27 16:55:47 -0500

    Improvements with responsiveness in Toggle Image Type page

--- src/scenes/dashboard/imager_settings.jsx
+++ src/scenes/dashboard/imager_settings.jsx
@@ -1,5 +1,7 @@
 import { Container, Row, Col } from "react-bootstrap";
 import React from "react";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import CommonRow from "../../components/CommonRow";
 import "../../CSS/imager_settings.css";
 // import { ReactComponent as DroneDiagram } from "../../assets/Drone-Diagram.svg";
@@ -30,14 +32,20 @@ function Imager_Settings() {
       <Row>
         <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12}>
           <div className="largeBox">
-            <select className="dropdown" defaultValue="00:00PM">
-              <option disabled>00:00PM</option>
-              <option>1:00PM</option>
-              <option>2:00PM</option>
-              <option>3:00PM</option>
-              <option>4:00PM</option>
-              <option>5:00PM</option>
-            </select>
+            <div className="dropdown-textbox-container">
+              <ArrowBackIosIcon className="arrowIcon" />
+              <input type="text" className="textbox" placeholder="00/00/23" />
+              <select className="dropdown" defaultValue="00:00PM">
+                <option disabled>00:00PM</option>
+                <option>1:00PM</option>
+                <option>2:00PM</option>
+                <option>3:00PM</option>
+                <option>4:00PM</option>
+                <option>5:00PM</option>
+              </select>
+              <button className="enterButton">Enter</button>
+              <ArrowForwardIosIcon className="arrowIcon" />
+            </div>
           </div>
         </Col>
       </Row>
