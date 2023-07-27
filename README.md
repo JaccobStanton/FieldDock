e014257e779266a414cf3663fe66dd74e9965e28
Jake Stanton <jaccobstanton@gmail.com>
2023-07-26 23:12:15 -0500

    started diagnostics page

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 
 Main Dashboard for FieldDock Project, built with ReactJS, Material UI ect...
 This version does not contain the sidebar
-
--- src/scenes/dashboard/imager_settings.jsx
+++ src/scenes/dashboard/imager_settings.jsx
@@ -2,13 +2,6 @@ import "../../CSS/imager_settings.css";
 import { Container, Row, Col } from "react-bootstrap";
 import React, { useState } from "react";
 import CommonRow from "../../components/NavBar/common_row";
-import EnvData from "../../components/Download_Data/env_data";
-import TraitData from "../../components/Download_Data/trait_data";
-import Fielddock_Data from "../../components/Download_Data/fielddock_data";
-import Mosaicked_Data from "../../components/Download_Data/mosaicked_data";
-import Drone_Data from "../../components/Download_Data/drone_data";
-import Sensor_Data from "../../components/Download_Data/sensor_data";
-import Download_All from "../../components/Download_Data/download_all";
 
 function Imager_Settings() {
   return (
@@ -20,9 +13,7 @@ function Imager_Settings() {
       <Row>
         <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12}>
           <div className="sensor-measurement-settings">
-            <div className="sensor-measurement-settings-title">
-              Download Data
-            </div>
+            <div className="sensor-measurement-settings-title">Diagnostics</div>
           </div>
         </Col>
       </Row>
@@ -30,7 +21,60 @@ function Imager_Settings() {
         {/* ------------------------------------------------- row 2 END ----------------------------- */}
       </Row>
       {/* ------------------------------------------------- row 3 END ----------------------------- */}
-      <Row></Row>
+      <Row>
+        <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12}>
+          <div className="diagnostics-log-parent">
+            <div className="diagnostics-log-box">
+              <h2 // Diagnostics Log title
+                style={{
+                  textAlign: "center",
+                  marginBottom: "20px",
+                }}
+              >
+                FieldDock Diagnostics Log
+              </h2>
+
+              <table // Table with bullet points and underlines
+                style={{
+                  width: "95%",
+                  borderCollapse: "collapse",
+                  textAlign: "left",
+                  fontSize: "16px",
+                  lineHeight: "1.5",
+                }}
+              >
+                <tbody>
+                  <tr>
+                    <td style={{ borderBottom: "1px solid #797979" }}>
+                      • 02/20/2021 / 13:45 / 0002:customer_1 / 0006: error_808.1
+                    </td>
+                  </tr>
+                  <tr>
+                    <td style={{ borderBottom: "1px solid #797979" }}>
+                      • 02/20/2021 / 14:45 / 0002:customer_1 / 0006: error_808.1
+                    </td>
+                  </tr>
+                  {/* <tr>
+                <td style={{ borderBottom: "1px solid #00e1b4" }}>• Item 3</td>
+              </tr>
+              <tr>
+                <td style={{ borderBottom: "1px solid #00e1b4" }}>• Item 3</td>
+              </tr>
+              <tr>
+                <td style={{ borderBottom: "1px solid #00e1b4" }}>• Item 3</td>
+              </tr>
+              <tr>
+                <td style={{ borderBottom: "1px solid #00e1b4" }}>• Item 3</td>
+              </tr>
+              <tr>
+                <td style={{ borderBottom: "1px solid #00e1b4" }}>• Item 3</td>
+              </tr> */}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </Col>
+      </Row>
       {/* ------------------------------------------------- row 4 END ----------------------------- */}
       <Row></Row>
       {/* ------------------------------------------------- row 5 END ----------------------------- */}
