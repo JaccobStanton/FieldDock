f1f277ebff8012ff209f14ed25fd8d29b0bd604e
JaccobStanton <jstanton23@my.stlcc.edu>
2023-09-06 16:09:45 -0500

    breadcrumbs added to last drone page

--- src/components/Tools/Breadcrumbs/missionBreadCrumbs.jsx
+++ src/components/Tools/Breadcrumbs/missionBreadCrumbs.jsx
@@ -1,69 +1,49 @@
 import * as React from "react";
-import Typography from "@mui/material/Typography";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Link from "@mui/material/Link";
-import HomeIcon from "@mui/icons-material/Home";
-import ChecklistIcon from "@mui/icons-material/Checklist";
+import TaskAltIcon from "@mui/icons-material/TaskAlt";
+import QueryStatsIcon from "@mui/icons-material/QueryStats";
 import CloudIcon from "@mui/icons-material/Cloud";
 import DescriptionIcon from "@mui/icons-material/Description";
 
-function handleClick(event) {
-  event.preventDefault();
-  console.info("You clicked a breadcrumb.");
-}
-
 export default function MissionBreadcrumbs() {
+  const [selected, setSelected] = React.useState(""); // 1. Use useState
+
+  function handleClick(event, breadcrumbName) {
+    // 2. Update handleClick
+    event.preventDefault();
+    console.info("You clicked a breadcrumb: " + breadcrumbName);
+    setSelected(breadcrumbName);
+  }
+
   return (
-    <div role="presentation" onClick={handleClick}>
+    <div role="presentation">
       <Breadcrumbs aria-label="breadcrumb">
-        <Link
-          underline="hover"
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            color: "#797979",
-            fontSize: "20px",
-          }}
-          href="/"
-        >
-          <HomeIcon sx={{ mr: 0.5 }} />
-          Status
-        </Link>
-        <Link
-          underline="hover"
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            color: "#797979",
-            fontSize: "20px",
-          }}
-          href="/"
-        >
-          <ChecklistIcon sx={{ mr: 0.5 }} />
-          Metrics
-        </Link>
-        <Typography
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            color: "#797979",
-            fontSize: "20px",
-          }}
-        >
-          <CloudIcon sx={{ mr: 0.5 }} />
-          Environment
-        </Typography>
-        <Typography
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            color: "#797979",
-            fontSize: "20px",
-          }}
-        >
-          <DescriptionIcon sx={{ mr: 0.5 }} />
-          Flight Logs
-        </Typography>
+        {["Status", "Metrics", "Environment", "Flight Logs"].map(
+          (breadcrumb, index) => (
+            <Link
+              key={breadcrumb}
+              underline="hover"
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                color: selected === breadcrumb ? "#00e1b4" : "#797979", // 3. Conditionally apply color
+                fontSize: "20px",
+                "&:hover": {
+                  color: "#00e1b4",
+                },
+              }}
+              href="/"
+              onClick={(e) => handleClick(e, breadcrumb)} // attach click event to each breadcrumb
+            >
+              {index === 0 && <TaskAltIcon sx={{ mr: 0.5 }} />}
+              {index === 1 && <QueryStatsIcon sx={{ mr: 0.5 }} />}
+              {index === 2 && <CloudIcon sx={{ mr: 0.5 }} />}
+              {index === 3 && <DescriptionIcon sx={{ mr: 0.5 }} />}
+              {breadcrumb}
+            </Link>
+          )
+        )}
       </Breadcrumbs>
     </div>
   );
--- src/scenes/dashboard/imager_settings.jsx
+++ src/scenes/dashboard/imager_settings.jsx
@@ -47,35 +47,55 @@ function Imager_Settings() {
                 backgroundColor: "transparent",
               }}
             >
-              {/* <MapComponent
-                onMapClick={onMapClick}
-                markerPositions={markerPositions}
-                setHomeLat={setHomeLat}
-                setHomeLng={setHomeLng}
-              /> */}
+              {/* <MapComponent /> */}
             </div>
           </Col>
-          <Col /*</div>xs={12} sm={6} md={6} lg={4} xl={4} xxl={4}> */>
+          <Col /*<div>xs={12} sm={6} md={6} lg={4} xl={4} xxl={4}> */>
             <div
               style={{
+                display: "flex", // Set the display to flex
+                flexDirection: "column", // Children are stacked vertically
                 height: "700px",
-                justifyContent: "center",
-                alignItems: "center",
+                alignItems: "center", // Center children horizontally
                 marginLeft: "15px",
                 border: "1px solid #797979",
                 backgroundColor: "transparent",
-                overflow: "auto", // This will add a scrollbar if the table exceeds the div's size
+                overflow: "auto",
               }}
             >
-              <div
+              <div //?DIV BOX FOR THE BREAD CRUMBS
                 style={{
-                  display: "flex", // Set the display to flex
-                  justifyContent: "center", // Center horizontally
-                  alignItems: "center", // Center vertically
+                  //bottom border does not go all the way across
+                  //     display: "flex",
+                  //     justifyContent: "center",
+                  //     alignItems: "center",
+                  //     borderBottom: "1px solid #797979",
+                  //     marginTop: "10px",
+                  //   }}
+                  // >
+                  // bottom border goes all the way across
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
                   borderBottom: "1px solid #797979",
+                  // marginTop: "10px",
+                  alignSelf: "stretch", // Make this div stretch to fill its parent's width
                 }}
               >
                 <MissionBreadcrumbs />
+              </div>
+              <div //?DIV BOX FOR MQTT COMMUNICATION
+                style={{
+                  height: "86%",
+                  border: "1px solid #797979",
+                  marginTop: "20px",
+                  width: "90%",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  display: "flex",
+                }}
+              >
+                hi
               </div>
             </div>
           </Col>
