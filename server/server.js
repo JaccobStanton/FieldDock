955ff71720777e31753665532e230619afcfc307
JaccobStanton <jstanton23@my.stlcc.edu>
2023-09-19 20:47:01 -0500

    node server added for mqtt troubleshooting

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mqtt = require("mqtt");
+const cors = require("cors"); // Import the CORS library
 
 const app = express();
 const port = 3001;
@@ -8,12 +9,16 @@ const client = mqtt.connect("mqtt://3.145.131.67:9001");
 
 let latestMessage = ""; // Store the latest message from MQTT
 
+// Use the cors middleware
+app.use(cors());
+
 client.on("connect", function () {
   console.log("Connected to MQTT broker");
   client.subscribe("drone/status");
 });
 
 client.on("message", (topic, message) => {
+  console.log(`Received message from ${topic}: ${message.toString()}`);
   latestMessage = message.toString();
 });
 
@@ -23,4 +28,8 @@ app.get("/latest", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
+});
+
+client.on("error", (error) => {
+  console.error("MQTT Client Error:", error);
 });
